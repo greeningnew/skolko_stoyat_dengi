afd6635695ae408187d78b7c70a4df5b52a2ab19
greeningnew <greening2025new@gmail.com>
2026-04-28 19:24:47 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,11 @@
-const CACHE_NAME = 'skolko-stoyat-dengi-v3';
-const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './assets/logo.svg'];
-
+const CACHE_NAME = 'skolko-stoyat-dengi-v5';
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './assets/logo.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
-      .then(() => self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
