4cc66b3fa09b68012e88c44ed19aaeae5c1a1ef7
greeningnew <greening2025new@gmail.com>
2026-05-03 00:22:30 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'skolko-stoyat-dengi-v12-ux';
+const CACHE_NAME = 'skolko-stoyat-dengi-v13-analytics';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './assets/logo.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
